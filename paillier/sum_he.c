2219061941f98db9bdc3ab8e0abff29a5223f82e
Jack <z_chiz@live.com>
2015-11-27 00:04:22 -0800

    sum_he in progress

--- paillier/sum_he.c
+++ paillier/sum_he.c
@@ -1,12 +1,93 @@
+#ifdef STANDARD
+/* STANDARD is defined, don't use any mysql functions */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#ifdef __WIN__
+typedef unsigned __int64 ulonglong;/* Microsofts 64 bit types */
+typedef __int64 longlong;
+#else
+typedef unsigned long long ulonglong;
+typedef long long longlong;
+#endif /*__WIN__*/
+#else
+#include <my_global.h>
+#include <my_sys.h>
+#if defined(MYSQL_SERVER)
+#include <m_string.h>/* To get strmov() */
+#else
+/* when compiled as standalone */
+#include <string.h>
+#define strmov(a,b) stpcpy(a,b)
+#define bzero(a,b) memset(a,0,b)
+#define memcpy_fixed(a,b,c) memcpy(a,b,c)
+#endif
+#endif
+#include <mysql.h>
+#include <ctype.h>
 
+#ifdef HAVE_DLOPEN
+
+#if !defined(HAVE_GETHOSTBYADDR_R) || !defined(HAVE_SOLARIS_STYLE_GETHOST)
+static pthread_mutex_t LOCK_hostname;
+#endif
+
+//////////////////////////////////////////////////////////////////
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <gmp.h>
+#include <paillier.h>
+
+
+// Main function
 char* sum_he(UDF_INIT *initid, UDF_ARGS *args, char *is_null, char *error);
 
+// Init/deinit
 my_bool sum_he_init(UDF_INIT *initid, UDF_ARGS *args, char *message);
 void sum_he_deinit(UDF_INIT *initid);
 
+// Reset/clear/add
 void sum_he_reset(UDF_INIT *initid, UDF_ARGS *args, char *is_null, char *error);
 void sum_he_clear(UDF_INIT *initid, char *is_null, char *error);
 void sum_he_add(UDF_INIT *initid, UDF_ARGS *args, char *is_null, char *error);
+
+const char *PUB_HEX = "ffffffffffffffff";
+
+void paillier_get_rand(void* buf, int len) {
+    unsigned char *c = (unsigned char *) buf;
+    int i;
+    for (i = 0; i < len; i++) {
+        c[i] = rand() % 256;
+    }
+}
+
+typedef struct {
+	paillier_ciphertext_t sum;
+    paillier_pubkey_t pub;
+} sum_he_t;
+
+void sum_he_t_init(sum_he_t *sh) {
+    // Set pub
+    sh->pub = *paillier_pubkey_from_hex(argv[1]);
+
+    // Set sum to 0
+    paillier_plaintext_t pt = *paillier_plaintext_from_ui(0);
+    sh->sum = *paillier_enc(NULL, pub, pt, paillier_get_rand);
+}
+
+void sum_he_t_deinit(sum_he_t *sh) {
+    // Clear pub
+    mpz_clear(sh->pub.n);
+    mpz_clear(sh->pub.n_squared);
+    mpz_clear(sh->pub.n_plus_one);
+    free(sh->pub);
+    
+    // Clear sum
+    mpz_clear(sh->sum.c);
+    free(sh->sum);
+}
+
 
 // Implementation
 
@@ -17,25 +98,41 @@ my_bool sum_he_init(UDF_INIT *initid, UDF_ARGS *args, char *message) {
     }
     
     if (args->arg_type[0] != STRING_RESULT) {
-        strcpy(message, "SUM_HE() requires a BLOB");
+        strcpy(message, "SUM_HE() requires a TEXT");
         return 1;
     }
     
-    initid->ptr = (char*) malloc(sizeof(mpz_t));
+    initid->ptr = malloc(sizeof(sum_he_t));
     if (!initid->ptr) {
         strcpy(message, "Couldn't allocate memory");
         return 1;
     }
-    
-    bzero(initid->ptr, sizeof(mpz_t));
-    
+
     return 0;
 }
 
 void sum_he_deinit(UDF_INIT *initid) {
-    if (initid->ptr) {
-        free(initid->ptr);
-    }
+    sum_he_t *sh = (sum_he_t *) initid->ptr;
+    sum_he_t_deinit(sh);
+    free(sh);
+}
+
+void sum_he_clear(UDF_INIT *initid, char *is_null, char *error) {
+    sum_he_t_init((sum_he_t *) initid->ptr);
+}
+
+void sum_he_add(UDF_INIT *initid, UDF_ARGS *args, char *is_null, char *error) {
+    sum_he_t *sh = (sum_he_t *) initid->ptr;
+    
+    // Convert arg to ciphertext
+    paillier_ciphertext_t ct;
+    mpz_init_set_str(ct.c, (char *) args->args[0], 16);
+    
+    // Multiply
+    paillier_ciphertext_t mul;
+    mpz_init(mul.c);
+    paillier_mul(sh->pub, &mul, &ct, sh->sum);
+    sh->sum = mul;
 }
 
 void sum_he_reset(UDF_INIT *initid, UDF_ARGS *args, char *is_null, char *error) {
@@ -43,15 +140,10 @@ void sum_he_reset(UDF_INIT *initid, UDF_ARGS *args, char *is_null, char *error) 
     sum_he_add(initid, args, is_null, error);
 }
 
-void sum_he_clear(UDF_INIT *initid, char *is_null, char *error) {
-    bzero(initid->ptr, sizeof(mpz_t));
-}
-
-void sum_he_add(UDF_INIT *initid, UDF_ARGS *args, char *is_null, char *error) {
-    // TODO: save into initid pointer
-}
-
 char* sum_he(UDF_INIT *initid, UDF_ARGS *args, char *is_null, char *error) {
-    mpz_t sum = (mpz_t*) initid->ptr;
-    // TODO extract mpz_t as string an return
+    sum_he_t *sh = (sum_he_t *) initid->ptr;
+    return mpz_get_str(NULL, 16, sh->sum->c);
 }
+
+//////////////////////////////////////////////////////////////////
+#endif /* HAVE_DLOPEN */
