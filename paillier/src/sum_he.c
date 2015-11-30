6b9c59c07fafb98e097704fc6af3d93749feb931
Jack <z_chiz@live.com>
2015-11-29 22:57:29 -0800

    SUM_HE improvements

--- paillier/src/sum_he.c
+++ paillier/src/sum_he.c
@@ -69,6 +69,7 @@ const char *PUB_HEX = "eece2bb7c56a58313bc022039a1184ad";
 typedef struct {
     paillier_pubkey_t *pub;
     paillier_ciphertext_t *sum;
+    int row_count;
 } sum_he_t;
 
 void sum_he_t_init(sum_he_t *sh) {
@@ -76,6 +77,8 @@ void sum_he_t_init(sum_he_t *sh) {
     sh->pub = paillier_pubkey_from_hex(PUB_HEX);
     // Set sum to 0
     sh->sum = paillier_create_enc_zero();
+    // Set row count to 0
+    sh->row_count = 0;
 }
 
 void sum_he_t_free(sum_he_t *sh) {
@@ -84,21 +87,39 @@ void sum_he_t_free(sum_he_t *sh) {
     free(sh);
 }
 
+/////////////
+// HELPERS //
+/////////////
+
+bool is_hex_string(char *str) {
+    int i = 0;
+    while (str[i]) {
+        if (!isxdigit(str[i])) {
+            return false;
+        }
+        i += 1;
+    }
+    return true;
+}
+
 ////////////////////
 // IMPLEMENTATION //
 ////////////////////
 
 my_bool sum_he_init(UDF_INIT *initid, UDF_ARGS *args, char *message) {
+    // Must be a single argument
     if (args->arg_count != 1) {
         strcpy(message, "SUM_HE() requires one argument");
         return 1;
     }
 
+    // Must be an argument of type STRING
     if (args->arg_type[0] != STRING_RESULT) {
-        strcpy(message, "SUM_HE() requires a TEXT");
+        strcpy(message, "SUM_HE() requires a STRING");
         return 1;
     }
-    
+
+    // Allocate memory, initialize struct
     initid->ptr = malloc(sizeof(sum_he_t));
     sum_he_t *sh = (sum_he_t *) initid->ptr;
     sum_he_t_init(sh);
@@ -106,33 +127,49 @@ my_bool sum_he_init(UDF_INIT *initid, UDF_ARGS *args, char *message) {
         strcpy(message, "Couldn't allocate memory");
         return 1;
     }
-    
-    // TODO:
-    initid->maybe_null = 1;
-    initid->max_length = 128;
 
+    // Result may be NULL
+    initid->maybe_null = 1;
+
+    // Initialization successful
     return 0;
 }
 
 void sum_he_deinit(UDF_INIT *initid) {
+    // Free allocated resources
     sum_he_t *sh = (sum_he_t *) initid->ptr;
     sum_he_t_free(sh);
 }
 
 void sum_he_clear(UDF_INIT *initid, char *is_null, char *error) {
     sum_he_t *sh = (sum_he_t *) initid->ptr;
-    // Set sum to 0
+    // Reset sum to 0
     paillier_freeciphertext(sh->sum);
     sh->sum = paillier_create_enc_zero();
+    // Reset row count to 0
+    sh->row_count = 0;
 }
 
 void sum_he_add(UDF_INIT *initid, UDF_ARGS *args, char *is_null, char *error) {
     sum_he_t *sh = (sum_he_t *) initid->ptr;
     
+    // NULL values and empty strings are invalid
+    if (args->args[0] == NULL || args->lengths[0] == 0) {
+        *error = 1;
+        return;
+    }
+    
     // Extract hex value from args
     char *hex_value = (char *) malloc(sizeof(char) * (args->lengths[0] + 1));
     memcpy(hex_value, args->args[0], args->lengths[0]);
-    hex_value[args->lengths[0]] = 0;  // Terminating null character
+    hex_value[args->lengths[0]] = 0;  // Insert terminating null character
+    
+    // Check hex_value for validity
+    if (!is_hex_string(hex_value)) {
+        *error = 1;
+        free(hex_value);
+        return;
+    }
 
     // Convert arg to ciphertext
     paillier_ciphertext_t *ct = (paillier_ciphertext_t *) malloc(sizeof(paillier_ciphertext_t));
@@ -146,25 +183,35 @@ void sum_he_add(UDF_INIT *initid, UDF_ARGS *args, char *is_null, char *error) {
     paillier_freeciphertext(sh->sum);
     paillier_freeciphertext(ct);
     sh->sum = mul;
-
-    // TODO: handle NULLs
+    
+    // Increment row count
+    sh->row_count += 1;
 }
 
 void sum_he_reset(UDF_INIT *initid, UDF_ARGS *args, char *is_null, char *error) {
+    // Function only required for older versions of MySQL
     sum_he_clear(initid, is_null, error);
     sum_he_add(initid, args, is_null, error);
 }
 
 char* sum_he(UDF_INIT *initid, UDF_ARGS *args, char *result, unsigned long *length, char *is_null, char *error) {
-    // TODO: handle empty set (return NULL)
     sum_he_t *sh = (sum_he_t *) initid->ptr;
-    char *hex_result = mpz_get_str(NULL, 16, sh->sum->c);
-    strcpy(result, hex_result);
-    *length = strlen(hex_result);
-    free(hex_result);
-    return result;
+    // Return result only if no errors occurred and row_count is greater than zero
+    if (*error != 1 && *is_null != 1 && sh->row_count > 0) {
+        char *hex_result = mpz_get_str(NULL, 16, sh->sum->c);
+        strcpy(result, hex_result);
+        *length = strlen(hex_result);
+        free(hex_result);
+        return result;
+    }
+    // Return NULL otherwise
+    else {
+        *is_null = 1;
+        return result;
+    }
 }
 
 //////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////
+
 #endif /* HAVE_DLOPEN */
